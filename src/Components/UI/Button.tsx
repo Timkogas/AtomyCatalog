f683eb2100af9b6ac97d27d35e89a5adc5c1183e
Trymedvedya <106478028+Trymedvedya@users.noreply.github.com>
2023-06-17 15:29:41 +0300

    .

---
+++ src/Components/UI/Button.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface ButtonProps {
+    children:string,
+    category:string,
+    handleClickFunction:()=>void
+}
+
+const Button: React.FC<ButtonProps> = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Button;
--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -7,14 +7,19 @@ interface MainPageProps {
 }
 
 const MainPage: React.FC<MainPageProps> = () => {
-    const products = useAppSelector(state=>state.products.products)
+    const products = useAppSelector(state => state.products.products)
     return (
         <div className={cls.main_page}>
             <div className={cls.catalog_new}>
-                {products.filter(el=>el.new===true).map(el=><ProductContainer name={el.name} note={el.note} korean={el.korean} price={el.price}/>)}
+                {products.filter(el => el.new === true).map(el => <ProductContainer name={el.name} note={el.note} korean={el.korean} price={el.price} />)}
             </div>
             <div className={cls.registry}>
-
+                <h1 className={cls.title}>АТОМИ КОРЕЯ</h1>
+                <span className={cls.subtitle}>Премиальное качество, доступное каждому!</span>
+                <p className={cls.text_info}>Зарегистрируйся в интернет-магазине АТОМИ, чтобы приобретать продукцию
+                    по минимальной цене, пользоваться бесплатной доставкой и принимать участие
+                    в мероприятиях и акциях от компании!</p>
+                    
             </div>
             <div className={cls.info}>
 
